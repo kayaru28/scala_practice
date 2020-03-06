77d9a13f5d9e37f0ad1600970ebaf65a88b2f6e1
kayaru <istorytale090415@gmail.com>
2020-03-06 21:29:36 +0900

    add 7-6

---
+++ 7-6_match.scala
@@ -0,0 +1,15 @@
+
+
+def for10(): Unit ={
+    var one=2
+    one match{
+        case 1 => println("one")
+        case 2 => println("two")
+        case 3 => println("three")
+    }
+
+}
+
+for10
+
+
