3ea7da24ef026282fab1ea94446d5dbd2c2e0671
kayaru <istorytale090415@gmail.com>
2020-03-08 14:58:47 +0900

    add A1 trait

---
+++ A1_class_trait.scala
@@ -0,0 +1,31 @@
+
+
+trait Iterator[A] {
+  def hasNext: Boolean
+  def next(): A
+}
+class IntIterator(to: Int) extends Iterator[Int] {
+  private var current = 0
+  override def hasNext: Boolean = current < to
+  override def next(): Int =  {
+    if (hasNext) {
+      val t = current
+      current += 1
+      t
+    } else 0
+  }
+}
+val iter = new IntIterator(10)
+
+for(x <- 5 to 10){
+    println(iter.next)
+}
+println(iter.next)
+
+
+
+
+
+
+
+
