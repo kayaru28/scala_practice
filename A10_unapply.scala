9b0cb2fba18c529b690955eca16ded3e16f921b8
kayaru <istorytale090415@gmail.com>
2020-03-14 23:08:29 +0900

    add unapply

---
+++ A10_unapply.scala
@@ -0,0 +1,14 @@
+
+
+
+object UserID{
+    def apply(name:String) = s"$name--generatedID--yyyymmdd"
+    def unapply(user_id:String):Option[String] = {
+        val stringArray: Array[String] = user_id.split("--")
+        if (stringArray.tail.nonEmpty) Some(stringArray.head) else None
+    }
+}
+
+val user = UserID("takuya")
+println(user)
+println(UserID(user))
